fce4b000e7caba757dedf6a6658048b91dc8f4dd
adityarana2610 <90079278+adityarana2610@users.noreply.github.com>
2023-11-02 20:50:42 +0530

    Add files via upload

---
+++ q19e.java
@@ -0,0 +1,15 @@
+class q19e
+{
+    public static void main()
+    {    
+        int a=1;
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(a++ +"\t "); 
+            }
+            System.out.println();
+        }
+    }
+}
