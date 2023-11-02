dff4c3fd432271edc44b96365f92932f75d1c8cd
adityarana2610 <90079278+adityarana2610@users.noreply.github.com>
2023-11-02 20:56:43 +0530

    Add files via upload

---
+++ name.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class name
+{
+  public static void main()
+  {
+    Scanner sc=new Scanner(System.in);
+    String s;
+    System.out.println("Enter your Name: ");
+    s=sc.nextLine();
+    System.out.println("Name is: "+s);
+  }
+}
